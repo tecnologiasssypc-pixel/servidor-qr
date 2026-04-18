e2ffd5d3605ff0d4626c937408690b17870655b3
teconologiasssypc-pixel <tecnologias.ssypc@morelos.gob.mx>
2026-04-18 09:12:11 -0600

    redireccionando al index.html

--- app.js
+++ app.js
@@ -13,12 +13,20 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const fs = require("fs");
+
+// crear carpeta uploads si no existe
+const uploadDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 // ======================
 // MULTER CONFIG
 // ======================
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);

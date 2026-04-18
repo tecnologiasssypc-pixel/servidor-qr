736011b762f6a4a0c4f4d22dbdc799cf822f8640
teconologiasssypc-pixel <tecnologias.ssypc@morelos.gob.mx>
2026-04-18 09:18:02 -0600

    redireccionando conexion a bd

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ const upload = multer({ storage });
 // BASE DE DATOS
 // ======================
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+  });
 
 db.connect((err) => {
   if (err) {

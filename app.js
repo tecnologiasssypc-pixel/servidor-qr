21215e73866858564a170d8d7f668710d3e70afa
teconologiasssypc-pixel <tecnologias.ssypc@morelos.gob.mx>
2026-04-18 09:02:04 -0600

    redireccionando al index.html

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
 
